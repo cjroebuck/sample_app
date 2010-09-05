1fb8c4e996b7cd7b701e89d2f1cad2f50ce72c80
cjroebuck <tophtfc@gmail.com>
2010-09-05 04:01:01 +0100

    User sign in done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup',  :to => 'users#new'
-  match '/chris',   :to => 'users#1'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,7 +9,7 @@ describe "Users" do
       it "should not make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",         :with => ""
+          fill_in "Username",     :with => ""
           fill_in "Email",        :with => ""
           fill_in "Password",     :with => ""
           fill_in "Confirmation", :with => ""
@@ -24,10 +24,10 @@ describe "Users" do
       it "should create a new user" do
         lambda do
            visit signup_path
-           fill_in "Name"         :with => "TestUser"
-           fill_in "Email"        :with => "TestEmail@example.com"
-           fill_in "Password"     :with => "password"
-           fill_in "Confirmation" :with => "password"
+           fill_in "Username",         :with => "TestUser"
+           fill_in "Email",       :with => "TestEmail@example.com"
+           fill_in "Password",     :with => "password"
+           fill_in "Confirmation", :with => "password"
            click_button
            response.should have_selector("div.flash.success", :content => "Welcome")
            response.should render_template('users/show')
@@ -35,4 +35,30 @@ describe "Users" do
        end
      end
   end
+  
+  describe "sign in/out" do
+
+      describe "failure" do
+        it "should not sign a user in" do
+          visit signin_path
+          fill_in :email,    :with => ""
+          fill_in :password, :with => ""
+          click_button
+          response.should have_selector("div.flash.error", :content => "Invalid")
+        end
+      end
+
+      describe "success" do
+        it "should sign a user in and out" do
+          user = Factory(:user)
+          visit signin_path
+          fill_in :email,    :with => user.email
+          fill_in :password, :with => user.password
+          click_button
+          controller.should be_signed_in
+          click_link "Sign out"
+          controller.should_not be_signed_in
+        end
+      end
+    end
 end
